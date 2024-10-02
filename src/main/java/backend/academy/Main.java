9be16ad4d12d1258fa5a53285fac462f65086473
2218390 <2218390@brunel.ac.uk>
2024-10-02 19:25:14 +0100

    checkstyle

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,5 +1,6 @@
 package backend.academy;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import lombok.experimental.UtilityClass;
@@ -9,13 +10,16 @@ public class Main {
     private final PrintStream out = System.out;
 
     public static void main(String[] args) {
-        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("input.txt");
-        if (inputStream == null) {
-            out.println("Файл input.txt не найден");
-            return;
-        }
+        try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("input.txt")) {
+            if (inputStream == null) {
+                out.println("Файл input.txt не найден");
+                return;
+            }
 
-        HangmanGame game = new HangmanGame(inputStream);
-        game.play();
+            HangmanGame game = new HangmanGame(inputStream);
+            game.play();
+        } catch (IOException e) {
+            out.println("Ошибка при закрытии ресурса: " + e.getMessage());
+        }
     }
 }
